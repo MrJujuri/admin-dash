60995b06bb8de69ceb0f71d6abadd1258d50da72
! DevJujuri <zuhrihazim1970@gmail.com>
2025-11-01 15:29:39 +0800

    Create ClientWrapper.tsx

---
+++ app/components/ClientWrapper.tsx
@@ -0,0 +1,21 @@
+import { motion } from "framer-motion";
+import PageTransition from "./PageTransition";
+
+export default function ClientWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <PageTransition />
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1.2, ease: "easeOut" }}
+      >
+        {children}
+      </motion.div>
+    </>
+  );
+}
